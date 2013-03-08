b952f74265c67e712f1a9e4855e18628437b5952
danm <writetodan@yahoo.com>
2013-03-08 18:45:20 +0000

    @damned crawl all internal pages - breaks on in-page links :(

--- robinson.rb
+++ robinson.rb
@@ -19,6 +19,7 @@ end
 
 
 class Page
+  include Comparable
   attr_reader :path
   def initialize(fetcher, path)
     @fetcher = fetcher
@@ -28,9 +29,22 @@ class Page
     page = Nokogiri::HTML(@fetcher.fetch(@path))
     links(page).select { |href| href.start_with?('/') || !href.start_with?('http') }
   end
+  
   def internal_pages
-    pages = []
-    internal_links.collect {|link| Page.new(@fetcher, link) }.uniq { |page| page.path }
+    puts "getting internal pages for: #{@path}"
+    not_me = internal_links.select{ |link| link != @path}
+    pages = not_me.collect {|link| Page.new(@fetcher, link) }.uniq
+    puts "new pages found: #{pages.size}"
+    pages
+  end
+  def <=>(other)
+    @path <=> other.path
+  end
+  def eql?(other)
+    @path == other.path
+  end
+  def hash
+    @path.hash
   end
   def to_s
     "Page: '#{@path}'"
@@ -41,8 +55,23 @@ class Page
   end
 end
 
-def pages_under page
-  page.internal_pages
+def pages_under(page, except_for = [page])
+  next_pages = [] 
+  puts "pages_under: #{page} except for #{except_for}"
+  
+  next_pages = (next_pages + page.internal_pages.select { |internal_page| 
+    puts "checking equality for #{internal_page}"
+    except_this_page = except_for.include?(internal_page)
+
+    !except_this_page
+  }).uniq
+
+
+  all_pages = (next_pages + except_for).uniq
+  puts "next pages: #{next_pages}"
+  next_pages.each { |next_page| all_pages = (all_pages + pages_under(next_page, all_pages)).uniq }
 end
+
+
 
 puts pages_under(Page.new(Fetcher.new(base), '/'))
