d7256cd2890f9ace40ac6367726899f0b09123fc
danm <writetodan@yahoo.com>
2013-03-08 16:04:30 +0000

    @damned some likkul objects to find links

--- robinson.rb
+++ robinson.rb
@@ -3,8 +3,30 @@
 require 'patron'
 require 'nokogiri'
 
-http = Patron::Session.new
 base = ARGV.first.to_s
-http.base_url = base
 puts "base: '#{base}'"
-puts http.get('/').body
+
+class Fetcher
+  def initialize(base)
+    @http = Patron::Session.new
+    @http.base_url = base
+  end
+  def fetch(path)
+    @http.get(path).body
+  end
+end
+
+
+class Page
+  def initialize(fetcher, path)
+    @fetcher = fetcher
+    @path = path
+  end
+  def links
+    @page = Nokogiri::HTML(@fetcher.fetch(@path))
+    @page.css('a').collect{ |a| a.attr('href') }
+  end
+end
+
+
+puts Page.new(Fetcher.new(base), '/').links
