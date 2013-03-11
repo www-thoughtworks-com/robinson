5c519baa5730821facbd8899a4d048551d94396b
danm <writetodan@yahoo.com>
2013-03-11 14:03:42 +0000

    @damned @torsten #605 automated link check - switch over to anemone

--- robinson.rb
+++ robinson.rb
@@ -1,77 +1,42 @@
 #!/bin/ruby
 
-require 'patron'
-require 'nokogiri'
-require 'set'
 
-base = ARGV.first.to_s
-puts "base: '#{base}'"
+address = ARGV.first.to_s
+puts "address: '#{address}'"
 
-class Fetcher
-  def initialize(base)
-    @http = Patron::Session.new
-    @http.base_url = base
-  end
-  def fetch(path)
-    @http.get(path).body
-  end
-end
+require 'anemone'
 
 
-class Page
-  include Comparable
-  attr_reader :path
-  def initialize(fetcher, path)
-    @fetcher = fetcher
-    @path = path
+class Link
+  def initialize(uri)
+    @uri = uri
   end
-  def internal_links
-    page = Nokogiri::HTML(@fetcher.fetch(@path))
-    links(page).select { |href| href.start_with?('/') || !href.start_with?('http') }
-  end
-  
-  def internal_pages
-    puts "getting internal pages for: #{@path}"
-    not_me = internal_links.select{ |link| link != @path}
-    pages = not_me.collect {|link| Page.new(@fetcher, link) }.uniq
-    puts "new pages found: #{pages.size}"
-    pages
-  end
-  def <=>(other)
-    @path <=> other.path
-  end
-  def eql?(other)
-    @path == other.path
-  end
-  def hash
-    @path.hash
-  end
-  def to_s
-    "Page: '#{@path}'"
+  def on_website?(address)
+    puts "#{host_and_port} vs #{host_and_port_of(address)}"
+    host_and_port == host_and_port_of(address)
   end
   private
-  def links(page)
-    page.css('a').collect{ |a| a.attr('href') }
+  def host_and_port_of(address)
+    address.include?(':') ? address : address + ':80'
+  end
+  def host_and_port
+    @uri.host + ':' + @uri.port.to_s
   end
 end
 
-def pages_under(page, except_for = [page])
-  next_pages = [] 
-  puts "pages_under: #{page} except for #{except_for}"
-  
-  next_pages = (next_pages + page.internal_pages.select { |internal_page| 
-    puts "checking equality for #{internal_page}"
-    except_this_page = except_for.include?(internal_page)
 
-    !except_this_page
-  }).uniq
-
-
-  all_pages = (next_pages + except_for).uniq
-  puts "next pages: #{next_pages}"
-  next_pages.each { |next_page| all_pages = (all_pages + pages_under(next_page, all_pages)).uniq }
+Anemone.crawl("http://#{address}") do |anemone|
+  anemone.focus_crawl { |page|
+    puts "focus_crawl: #{page}"
+    page.links.each { |link| puts "any page link: #{link}" }
+    links = page.links.select { |uri|
+      link = Link.new(uri)
+      link.on_website?(address)
+    }
+    links.each { |link| puts "focus on link (internal): #{link}" }
+    links
+  }
+  anemone.on_every_page { |page|
+    puts "on every page: #{page.url}"
+  }
 end
-
-
-
-puts pages_under(Page.new(Fetcher.new(base), '/'))
